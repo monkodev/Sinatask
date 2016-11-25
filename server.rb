cf6a7f216b83dc6a8266fddee6be7bcec88fddf0
MonKoDev Technologies <gmalugani@gmail.com>
2016-11-25 18:09:32 -0300

    Actualiza index1.erb

--- server.rb
+++ server.rb
@@ -1,8 +1,15 @@
 require 'sinatra'
 require 'make_todo'
 
+
+# sets root as the parent-directory of the current file
+set :root, File.join(File.dirname(__FILE__), '')
+# sets the view directory correctly
+set :views, Proc.new { File.join(root, "views") } 
+
+
 get '/' do
-	erb :index	
+	erb :index1
 end
 
 get '/nuevatarea' do
