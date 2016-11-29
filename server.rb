28e65111ac638d5ce27923f7e90143d3bd172641
MonKoDev Technologies <gmalugani@gmail.com>
2016-11-29 16:19:43 -0300

    Crea eliminatareas.erb y actualiza server.rb

--- server.rb
+++ server.rb
@@ -19,3 +19,23 @@ get '/listarcompletadas' do
 	h=Tarea.all
 	erb :listarcompletadas, :locals => {:ho => h}
 end
+
+get '/listarsincompletar' do
+	h=Tarea.all
+	erb :listarsincompletar, :locals => {:ho => h}
+end
+
+get '/eliminartarea' do
+	msg=""
+	erb :eliminartarea, :locals => {:msgx => msg}
+end
+
+get '/deletetask' do
+	if Tarea.find(params[:deletask].to_i)==true
+		Tarea.destroy(params[:deletask])
+		msg="Tarea eliminada correctamente"
+	else
+		msg="No existe la tarea ingresada"
+	end
+	erb :eliminartarea, :locals => {:msgx => msg}	
+end
